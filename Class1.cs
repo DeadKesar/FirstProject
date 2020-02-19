4e56c2aa37cd06a0c687de8a0c86b9b55e0c68ef
DeadKesar <50805926+DeadKesar@users.noreply.github.com>
2020-02-19 08:22:58 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public abstract class Product
+{
+	string Name { get; set; }
+	double Quntity { get
+		{ this += value; } 
+			set; }
+	object Image { get; set; }
+	string Address { get; set; }
+	string Articul { get; set; }
+	public Class1( string name, string addres, string artiqul,object image, double quantity = 0)
+	{
+		this.Name = name;
+		this.Address = addres;
+		this.Articul = artiqul;
+		this.Image = image;
+		this.Quntity = quantity;
+	}
+}
